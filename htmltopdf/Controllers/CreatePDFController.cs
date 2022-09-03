dd96b4535c679a7ef15b3056fcfec63ca9df8388
Rishabh <rishabh9690@gmail.com>
2022-09-03 12:28:15 +0530

    Add project files.

---
+++ htmltopdf/Controllers/CreatePDFController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+using System;
+
+namespace htmltopdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreatePDFController : ControllerBase
+    {
+        [HttpPost]  
+        public IActionResult PDF()
+        {
+
+            HtmlToPdf htmlToPdf = new HtmlToPdf();
+
+            string path = "#####"; // Path from which the file need to be read 
+            string html = System.IO.File.ReadAllText(path);
+            //    PdfDocument doc =  htmlToPdf.ConvertUrl(url);
+            PdfDocument doc = htmlToPdf.ConvertHtmlString(html);
+           
+            doc.Save("Document.pdf");
+            byte[] bytes = doc.Save();
+            doc.Close();
+
+            return Ok("Done");
+        }
+
+    }
+}
